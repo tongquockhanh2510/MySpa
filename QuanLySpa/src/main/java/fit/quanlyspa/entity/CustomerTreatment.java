b5dbdc3c4ebe5a9a092c7f6e4e37bec21358ecd1
tongquockhanh2510 <22677951.khanh@student.iuh.edu.vn>
2026-01-25 00:30:36 +0700

    fix docker

--- QuanLySpa/src/main/java/fit/quanlyspa/entity/CustomerTreatment.java
+++ QuanLySpa/src/main/java/fit/quanlyspa/entity/CustomerTreatment.java
@@ -6,7 +6,6 @@ import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Getter
--- QuanLySpa/src/main/java/fit/quanlyspa/entity/Promotion.java
+++ QuanLySpa/src/main/java/fit/quanlyspa/entity/Promotion.java
@@ -6,7 +6,7 @@ import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+
 import java.time.LocalDateTime;
 
 @Entity
